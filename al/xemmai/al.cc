1c69776c809d2ef519bb9bc90236b25e104a4c70
Shin-ichi MORITA <shin1morita@gmail.com>
2011-09-20 14:03:30 +0000

    Made module factory portable.
    git-svn-id: https://al-xemmai.googlecode.com/svn/trunk@9 e225f556-057e-8d93-2721-d8bf3a3e47d8

--- al/xemmai/al.cc
+++ al/xemmai/al.cc
@@ -232,11 +232,7 @@ void t_extension::f_scan(t_scan a_scan)
 
 }
 
-#ifdef _MSC_VER
-extern "C" __declspec(dllexport) xemmai::t_extension* f_factory(xemmai::t_object* a_module)
-#else
-extern "C" xemmai::t_extension* f_factory(xemmai::t_object* a_module)
-#endif
+XEMMAI__MODULE__FACTORY(xemmai::t_object* a_module)
 {
 	return new al::xemmai::t_extension(a_module);
 }
